8382055148c3b3b27473320c80960d81726b19f6
tmcgroul <safonov.a.r@mail.ru>
2024-12-10 18:08:10 +0300

    fix first_block after restart

--- crates/archive/src/main.rs
+++ crates/archive/src/main.rs
@@ -1,17 +1,18 @@
 use clap::Parser;
 use ingest::{ingest_from_service, BlockRange};
-use sink::Sink;
 use layout::ChunkWriter;
+use sink::Sink;
+use sqd_data::solana::tables::SolanaChunkBuilder;
 use writer::ParquetWriter;
 
-use sqd_data::solana::tables::SolanaChunkBuilder;
 
+mod cli;
 mod fs;
 mod ingest;
-mod sink;
 mod layout;
+mod sink;
 mod writer;
-mod cli;
+
 
 fn chunk_check(filelist: &[String]) -> bool {
     for file in filelist {
@@ -21,6 +22,7 @@ fn chunk_check(filelist: &[String]) -> bool {
     }
     return false;
 }
+
 
 fn main() -> anyhow::Result<()> {
     let args = cli::Cli::parse();
@@ -32,12 +34,27 @@ fn main() -> anyhow::Result<()> {
     let runtime = std::rc::Rc::new(runtime);
     let _guard = runtime.enter();
 
-    let range = BlockRange::new(args.first_block, args.last_block);
-    let stream = ingest_from_service(&args.src, &range)?;
     let fs = fs::create_fs(&args.dest, runtime.clone())?;
     let chunk_builder = Box::new(SolanaChunkBuilder::new());
     let writer = ParquetWriter::new(chunk_builder);
-    let chunk_writer = ChunkWriter::new(&fs, chunk_check, args.first_block, args.last_block, args.top_dir_size)?;
+    let chunk_writer = ChunkWriter::new(
+        &fs,
+        chunk_check,
+        args.first_block,
+        args.last_block,
+        args.top_dir_size,
+    )?;
+    let first_block = chunk_writer.next_block();
+
+    if let Some(last_block) = args.last_block {
+        if first_block > last_block {
+            println!("nothing to do");
+            return Ok(());
+        }
+    }
+
+    let range = BlockRange::new(first_block, args.last_block);
+    let stream = ingest_from_service(&args.src, &range)?;
     let mut sink = Sink::new(writer, chunk_writer, &fs, args.chunk_size);
     sink.write(stream)?;
 
