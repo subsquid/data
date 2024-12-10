251be173934456700958cb4e88328012c4fc8dfa
tmcgroul <safonov.a.r@mail.ru>
2024-12-10 15:35:51 +0300

    introduce cli

--- crates/archive/src/main.rs
+++ crates/archive/src/main.rs
@@ -1,3 +1,4 @@
+use clap::Parser;
 use ingest::{ingest_from_service, BlockRange};
 use sink::Sink;
 use layout::ChunkWriter;
@@ -22,14 +23,14 @@ fn chunk_check(filelist: &[String]) -> bool {
 }
 
 fn main() -> anyhow::Result<()> {
-    let range = BlockRange::new(250_000_000, Some(250_001_000));
-    let stream = ingest_from_service("http://localhost:7373", &range)?;
-    let fs = fs::create_fs("data/solana")?;
+    let args = cli::Cli::parse();
+    let range = BlockRange::new(args.first_block, args.last_block);
+    let stream = ingest_from_service(&args.src, &range)?;
+    let fs = fs::create_fs(&args.dest)?;
     let chunk_builder = Box::new(SolanaChunkBuilder::new());
     let writer = ParquetWriter::new(chunk_builder);
-    let chunk_writer = ChunkWriter::new(&fs, chunk_check, 250_000_000, Some(250_001_000), 4096)?;
-    let chunk_size = 256;
-    let mut sink = Sink::new(writer, chunk_writer, &fs, chunk_size);
+    let chunk_writer = ChunkWriter::new(&fs, chunk_check, args.first_block, args.last_block, args.top_dir_size)?;
+    let mut sink = Sink::new(writer, chunk_writer, &fs, args.chunk_size);
     sink.write(stream)?;
 
     Ok(())
