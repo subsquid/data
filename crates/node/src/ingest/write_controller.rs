394f1603e87878751601f1af2991b4bbbb076cf0
Eldar Gabdullin <eldargab@gmail.com>
2024-12-22 18:55:35 +0400

    save work

--- crates/node/src/ingest/write_controller.rs
+++ crates/node/src/ingest/write_controller.rs
@@ -1,26 +1,36 @@
-use crate::types::DBRef;
+use crate::types::{DBRef, DatasetKind};
+use anyhow::ensure;
+use either::Either;
 use sqd_data_client::BlockRef;
+use sqd_data_core::BlockNumber;
 use sqd_storage::db::{Chunk as StorageChunk, DatasetId};
 
 
 pub struct WriteController {
     db: DBRef,
     dataset_id: DatasetId,
-    base: BlockRef,
-    head: BlockRef,
-    finalized_head: BlockRef,
+    dataset_kind: DatasetKind,
+    first_block: BlockNumber,
+    head: Option<BlockRef>,
+    finalized_head: Option<BlockRef>,
 }
 
 
 impl WriteController {
-    pub fn head(&self) -> &BlockRef {
-        &self.head
+    pub fn head_hash(&self) -> Option<&str> {
+        self.head.as_ref().map(|h| h.hash())
+    }
+    
+    pub fn next_block(&self) -> BlockNumber {
+        self.head.as_ref().map_or(self.first_block, |h| h.number() + 1)
     }
 
-    pub fn rollback(&mut self, prev: &[BlockRef]) -> anyhow::Result<BlockRef> {
-        if prev.is_empty() {
-            return Ok(self.head.clone())
-        }
+    pub fn compute_rollback(&self, prev: &[BlockRef]) -> anyhow::Result<Either<BlockRef, BlockNumber>> {
+        ensure!(!prev.is_empty(), "no previous blocks where provided");
+        ensure!(
+            prev.windows(2).all(|s| s[0].number() < s[1].number()),
+            "list of previous blocks is not ordered"
+        );
 
         let snapshot = self.db.snapshot();
 
@@ -45,25 +55,26 @@ impl WriteController {
             
             if let Some(&block) = prev_blocks.peek() {
                 if block.number() == head.last_block && block.hash() == &head.last_block_hash {
-                    return Ok(block.clone())
+                    return Ok(Either::Left(block.clone()))
                 }
             } else {
                 return Ok(
-                    BlockRef::new(head.last_block, &head.last_block_hash)
+                    Either::Left(BlockRef::new(head.last_block, &head.last_block_hash))
                 )
             }
         }
 
-        Ok(self.base.clone())
+        Ok(Either::Right(self.first_block))
     }
 
     pub fn insert_new_chunk(&mut self, chunk: &StorageChunk) -> anyhow::Result<()> {
-        if self.head.number() < chunk.first_block {
+        if self.next_block() <= chunk.first_block {
+            ensure!(self.next_block() == chunk.first_block);
             self.db.insert_chunk(self.dataset_id, chunk)?;
         } else {
             self.db.insert_fork(self.dataset_id, chunk)?;
         }
-        self.head = BlockRef::new(chunk.last_block, &chunk.last_block_hash);
+        self.head = Some(BlockRef::new(chunk.last_block, &chunk.last_block_hash));
         Ok(())
     }
 
