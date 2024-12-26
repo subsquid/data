2eabce2a06e8f775e044c4c7fb5debca52febdab
Denis Kanonik <denisbsu@gmail.com>
2024-12-26 17:58:13 +0000

    Fix lines split

--- crates/data-client/src/reqwest/lines.rs
+++ crates/data-client/src/reqwest/lines.rs
@@ -5,31 +5,33 @@ use futures_core::Stream;
 use std::pin::Pin;
 use std::task::Poll;
 
-
 pub struct LineStream {
     inner: Option<BodyStreamBox>,
     line: BytesMut,
     unchecked_pos: usize,
 }
 
-
 impl LineStream {
     pub fn new(inner: BodyStreamBox) -> Self {
         Self {
             inner: Some(inner),
             line: BytesMut::new(),
-            unchecked_pos: 0
+            unchecked_pos: 0,
         }
     }
-    
+
     fn check_line(&mut self) -> Option<Bytes> {
-        if let Some(pos) = self.line.as_ref()[self.unchecked_pos..].iter().position(|b| *b == b'\n') {
-            let line = self.line.split_to(pos).freeze();
-            self.line.advance(if self.line.get(2).copied() == Some(b'\r') {
-                2
-            } else {
-                1
-            });
+        if let Some(pos) = self.line.as_ref()[self.unchecked_pos..]
+            .iter()
+            .position(|b| *b == b'\n')
+        {
+            let line = self.line.split_to(self.unchecked_pos + pos).freeze();
+            self.line
+                .advance(if self.line.get(2).copied() == Some(b'\r') {
+                    2
+                } else {
+                    1
+                });
             self.unchecked_pos = 0;
             Some(line)
         } else {
@@ -37,7 +39,7 @@ impl LineStream {
             None
         }
     }
-    
+
     fn take_final_line(&mut self) -> Option<Bytes> {
         let line = std::mem::take(&mut self.line);
         if line.is_empty() {
@@ -48,29 +50,27 @@ impl LineStream {
     }
 }
 
-
 impl Stream for LineStream {
     type Item = reqwest::Result<Bytes>;
 
-    fn poll_next(self: Pin<&mut Self>, cx: &mut std::task::Context<'_>) -> Poll<Option<Self::Item>> {
+    fn poll_next(
+        self: Pin<&mut Self>,
+        cx: &mut std::task::Context<'_>,
+    ) -> Poll<Option<Self::Item>> {
         let this = self.get_mut();
-        
+
         loop {
             if let Some(line) = this.check_line() {
-                return Poll::Ready(Some(Ok(line)))
+                return Poll::Ready(Some(Ok(line)));
             }
-            
+
             if let Some(inner) = this.inner.as_mut() {
                 match Pin::new(inner).poll_next(cx) {
                     Poll::Ready(None) => {
                         this.inner = None;
-                        return Poll::Ready(
-                            Ok(this.take_final_line()).transpose()
-                        )
-                    },
-                    Poll::Ready(Some(Ok(bytes))) => {
-                        this.line.extend_from_slice(&bytes)
-                    },
+                        return Poll::Ready(Ok(this.take_final_line()).transpose());
+                    }
+                    Poll::Ready(Some(Ok(bytes))) => this.line.extend_from_slice(&bytes),
                     Poll::Ready(Some(Err(err))) => {
                         this.inner = None;
                         this.line = BytesMut::new();
@@ -78,12 +78,12 @@ impl Stream for LineStream {
                             Poll::Ready(None)
                         } else {
                             Poll::Ready(Some(Err(err)))
-                        }
-                    },
+                        };
+                    }
                     Poll::Pending => return Poll::Pending,
-                }   
+                }
             } else {
-                return Poll::Ready(None)
+                return Poll::Ready(None);
             }
         }
     }
