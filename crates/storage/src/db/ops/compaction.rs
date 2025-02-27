efc8c9e1e47806ce3088a9c6a9f63eea508a07e2
Denis Kanonik <denisbsu@gmail.com>
2025-02-27 16:03:19 +0300

    Add compaction planner

--- crates/storage/src/db/ops/compaction.rs
+++ crates/storage/src/db/ops/compaction.rs
@@ -3,19 +3,22 @@ use crate::db::ops::table_merge::TableMerge;
 use crate::db::table_id::TableId;
 use crate::db::write::tx::Tx;
 use crate::db::{Chunk, ChunkReader, DatasetId, ReadSnapshot, TableBuilder};
+use core::u64;
 use std::collections::BTreeMap;
 
+pub const MIN_CHUNK_SIZE: u64 = 100;
 
 pub enum CompactionStatus {
     Ok,
-    Canceled
+    Canceled,
+    NotingToCompact
 }
 
 
 pub fn perform_dataset_compaction(db: &RocksDB, dataset_id: DatasetId) -> anyhow::Result<CompactionStatus> {
     DatasetCompaction {
         db,
-        _snapshot: &ReadSnapshot::new(db),
+        snapshot: &ReadSnapshot::new(db),
         dataset_id,
         merge: Vec::new()
     }.execute()
@@ -24,7 +27,7 @@ pub fn perform_dataset_compaction(db: &RocksDB, dataset_id: DatasetId) -> anyhow
 
 struct DatasetCompaction<'a> {
     db: &'a RocksDB,
-    _snapshot: &'a ReadSnapshot<'a>,
+    snapshot: &'a ReadSnapshot<'a>,
     dataset_id: DatasetId,
     merge: Vec<ChunkReader<'a>>
 }
@@ -33,6 +36,10 @@ struct DatasetCompaction<'a> {
 impl<'a> DatasetCompaction<'a> {
     fn execute(mut self) -> anyhow::Result<CompactionStatus> {
         self.prepare_merge_plan()?;
+
+        if self.merge.len() < 2 {
+            return Ok(CompactionStatus::NotingToCompact);
+        }
 
         let new_chunk = {
             let mut tables = BTreeMap::new();
@@ -122,6 +129,32 @@ impl<'a> DatasetCompaction<'a> {
     }
 
     fn prepare_merge_plan(&mut self) -> anyhow::Result<()> {
-        todo!()
+        let mut reversed_chunk_iterator = self.snapshot.list_chunks(self.dataset_id, 0, None).into_reversed();
+        let mut step_back = false;
+        let mut first_applicable_block = u64::MAX;
+        while let Some(Ok(el)) = reversed_chunk_iterator.next() {
+            if el.blocks_count() >= MIN_CHUNK_SIZE as u64 {
+                step_back = true;
+                break;
+            }
+            first_applicable_block = el.first_block();
+        };
+        let mut chunk_iterator = self.snapshot.list_chunks(self.dataset_id, first_applicable_block, None);
+        let mut new_chunk_size: u64 = 0;
+        let mut expected_first_block = first_applicable_block;
+        while let Some(Ok(chunk)) = chunk_iterator.next() {
+            if chunk.first_block() != expected_first_block {
+                break;
+            }
+            expected_first_block = chunk.next_block();
+            new_chunk_size += chunk.blocks_count();
+            let reader = self.snapshot.create_chunk_reader(chunk);
+            self.merge.push(reader);
+            if new_chunk_size >= MIN_CHUNK_SIZE {
+                break;
+            }
+        };
+
+        Ok(())
     }
 }
