e0563928cf73717b800e9e13a1936cd33b637b42
Eldar Gabdullin <eldargab@gmail.com>
2025-05-10 18:30:41 +0400

    fix retain() call condition to not trigger needless call

--- crates/node/src/ingest/dataset_controller.rs
+++ crates/node/src/ingest/dataset_controller.rs
@@ -163,7 +163,7 @@ impl WriteCtx {
                 self.notify_head();
                 self.notify_finalized_head();
                 if let Some(n) = head {
-                    if self.write.first_chunk_head().map_or(false, |h| self.write.next_block() - h.number >= n) {
+                    if self.write.first_chunk_head().map_or(false, |h| self.write.next_block() - h.number > n) {
                         self.retain(self.write.next_block() - n, None)?;
                     }
                 }
