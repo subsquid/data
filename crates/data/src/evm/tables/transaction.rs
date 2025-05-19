a930d6fa7028e8f0521742f6f21fec30d5983b7c
Denis Kanonik <denisbsu@gmail.com>
2025-05-19 10:55:18 +0300

    update sort_key for transactions

--- crates/data/src/evm/tables/transaction.rs
+++ crates/data/src/evm/tables/transaction.rs
@@ -62,7 +62,7 @@ table_builder! {
     description(d) {
         d.downcast.block_number = vec!["block_number"];
         d.downcast.item_index = vec!["transaction_index"];
-        d.sort_key = vec!["block_number", "transaction_index"];
+        d.sort_key = vec!["sighash", "to", "block_number", "transaction_index"];
         d.options.add_stats("block_number");
         d.options.add_stats("transaction_index");
         d.options.add_stats("to");
