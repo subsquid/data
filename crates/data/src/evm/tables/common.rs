9db54d3bcd5b6aad35028f434a1ae7b4067e9a5e
tmcgroul <safonov.a.r@mail.ru>
2026-04-29 19:26:43 +0300

    add new columns to evm block and transaction

--- crates/data/src/evm/tables/common.rs
+++ crates/data/src/evm/tables/common.rs
@@ -4,6 +4,8 @@ use sqd_array::builder::{ListBuilder, StringBuilder, UInt32Builder};
 pub type HexBytesBuilder = StringBuilder;
 pub type BlobHashesListBuilder = ListBuilder<HexBytesBuilder>;
 pub type TraceAddressListBuilder = ListBuilder<UInt32Builder>;
+pub type UncleListBuilder = ListBuilder<HexBytesBuilder>;
+pub type StorageKeyListBuilder = ListBuilder<HexBytesBuilder>;
 
 
 pub fn sighash(bytes: &str) -> Option<&str> {
